1b53dc432e8c14d074db13493c64aaed6549c125
goshaov <gosha.ovnanyan@yandex.ru>
2023-12-25 20:44:15 +0300

    подготовил к публикации

--- gulpfile.js
+++ gulpfile.js
@@ -13,17 +13,31 @@ function serve() {
 }
 
 function html() {
-  return gulp.src('src/**/*.html')
+  return gulp.src('src/*.html')
         .pipe(plumber())
 				.pipe(gulp.dest('dist/'))
         .pipe(browserSync.reload({stream: true}));
 }
 
 function css() {
-  return gulp.src('src/blocks/**/*.css')
+  return gulp.src('src/styles/*.css')
         .pipe(plumber())
         .pipe(concat('bundle.css'))
 				.pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+function fonts() {
+  return gulp.src('src/fonts/*.{css,woff}')
+        .pipe(plumber())
+				.pipe(gulp.dest('dist/fonts'))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+function scripts() {
+  return gulp.src('src/scripts/*.js')
+        .pipe(plumber())
+				.pipe(gulp.dest('dist/scripts'))
         .pipe(browserSync.reload({stream: true}));
 }
 
@@ -38,17 +52,19 @@ function clean() {
 }
 
 function watchFiles() {
-  gulp.watch(['src/**/*.html'], html);
-  gulp.watch(['src/blocks/**/*.css'], css);
+  gulp.watch(['src/*.html'], html);
+  gulp.watch(['src/style/*.css'], css);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images));
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts, scripts));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.html = html;
 exports.css = css;
 exports.images = images;
+exports.fonts = fonts;
+exports.scripts =scripts;
 exports.clean = clean;
 
 exports.build = build;
